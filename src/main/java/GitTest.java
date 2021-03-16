f19827e621c913fd51dd9a0a7cc9a0103ef759e9
serhatyesilmen <serhatyesilmen7272@gmail.com>
2021-03-16 18:51:46 -0500

    change added

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -2,7 +2,8 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("100_000_000");
                 System.out.println("10");
+        System.out.println("Serhat");
 
-        
+
     }
 }
